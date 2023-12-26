6b94f4609c85f43f6b2a819fd8ca55b43fd089a5
Sweet3214 <137040123+Sweet3214@users.noreply.github.com>
2023-12-26 11:22:50 +0000

    调用接口连接查询界面

--- src/pages/search.js
+++ src/pages/search.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { parse } from 'query-string';
 
 import Breadcrumbs from '../components/Breadcrumbs';
@@ -11,35 +11,55 @@ import { generateMockProductData } from '../helpers/mock';
 import * as styles from './search.module.css';
 
 const SearchPage = (props) => {
-  const params = parse(props.location.search);
-  const searchQuery = params.q ? params.q : '';
+  const [searchResulets,setSearchResults]= useState([]);
+  const params = parse(props.location.search);
+  const searchQuery = params.q ? params.q : '';
 
-  const sampleData = generateMockProductData(3, 'woman');
+  
+  const sampleData = generateMockProductData(3, 'woman');
 
-  return (
-    <Layout>
-      <div className={styles.root}>
-        <Container size={'large'} spacing={'min'}>
-          <Breadcrumbs
-            crumbs={[
-              { link: '/', label: 'Home' },
-              { label: `Search results for '${searchQuery}'` },
-            ]}
-          />
-          <div className={styles.searchLabels}>
-            <h4>Search results for '{searchQuery}'</h4>
-            <span>3 results</span>
-          </div>
-          <ProductCardGrid
-            showSlider={false}
-            height={580}
-            columns={3}
-            data={sampleData}
-          />
-        </Container>
-      </div>
-    </Layout>
-  );
+
+  useEffect(()=>{
+    const fetchData= async ()=>{
+      try{
+        const response = await fetch ('fastapi后端地址')
+        const data = await response.json();
+        setSearchResults(data.products);
+      }catch (error){
+        console.error('Error fetching search results:',error);
+      }
+    };
+    fetchData();
+  
+  },[searchQuery]);
+
+
+  
+  return (
+    <Layout>
+      <div className={styles.root}>
+        <Container size={'large'} spacing={'min'}>
+          <Breadcrumbs
+            crumbs={[
+              { link: '/', label: 'Home' },
+              { label: `Search results for '${searchQuery}'` },
+            ]}
+          />
+          <div className={styles.searchLabels}>
+            <h4>Search results for '{searchQuery}'</h4>
+            <span>3 results</span>
+          </div>
+          <ProductCardGrid
+            showSlider={false}
+            height={580}
+            columns={3}
+            data={sampleData}
+          />
+        </Container>
+      </div>
+    </Layout>
+  );
 };
 
 export default SearchPage;
+
